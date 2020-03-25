4f657d93e7343f451762eb0458c35f033294e479
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-03-25 16:42:03 -0400

    all done except stretch goal function WIP

--- main.js
+++ main.js
@@ -3,48 +3,52 @@ const myCodeImmersivesObj = {
 };
 
 // Set the value of property `objectify` to be "yourself".
-
+myCodeImmersivesObj.objectify = 'yourself';
 
 // Add the new property `isAnExcellentObject` and set it to `true`.
-
+myCodeImmersivesObj.isAnExcellentObject = 'true';
 
 // Add the property `term` and set it to 1.
-
+myCodeImmersivesObj.term = 1;
 
 // Now add 1 to the value of your object's `term` property and put that new value back in your object's `term`.
-
-
+myCodeImmersivesObj.term = myCodeImmersivesObj.term + 1
 // Now add a new property called `currentScore` and set it to 3.
-
+myCodeImmersivesObj.currentScore = 3;
 
 // Now add a new property called `cumulativeScore` and set it to 50.
-
+myCodeImmersivesObj.cumulativeScore = 50;
 
 // Now add your `currentScore` and your `cumulativeScore` and put it back in `cumulativeScore`.
-
+myCodeImmersivesObj.cumulativeScore = myCodeImmersivesObj.currentScore + myCodeImmersivesObj.cumulativeScore
 
 // Now add a property called `I am exactly this cool` (you'll need bracket notation!) and set it to `soooooo cool`.
+myCodeImmersivesObj[ 'I am exactly this cool' ] = `soooooo cool`;
 
 
 // Now make a new EMPTY object that represents yourself.
-
+const myNewObj = {}
 
 // Add a `firstName` property and set it to a string.
-
+myNewObj.firstName = 'Bogdan';
 
 // Add a `lastName` property and set it to a string.
-
+myNewObj.lastName = 'Kowaltchook'
 
 // Add a `fullName` property and set it to the name properties put together with a space string in the middle.
-
+myNewObj.fullName = myNewObj.firstName + ' ' + myNewObj.lastName
 
 // Add an `age` property and set it to a number.
-
+myNewObj.age = 34;
 
 // Add an address property and set it to an empty array.
-
+myNewObj.address = [];
 
 // Now push three strings into that array.
+myNewObj.address.push('New York')
+myNewObj.address.push('Brooklyn')
+myNewObj.address.push('SomeWhere OverThere Address')
+
 
 
 // Now make a new object, but this time we'll pre-populate it,
@@ -58,6 +62,17 @@ const myCodeImmersivesObj = {
 // bracket notation as well. The important thing here is: this
 // should all be done WITHIN the brackets of the object, NOT
 // below the declaration of the object.
+const myOwnObj = {
+  name: 'Tesla',
+  cat: true,
+  age: 5,
+  likes: 'Playing with a String',
+  FavouriteNums: [3, 6, 9],
+  ['bracket notation example']: true
+}
 
 
 // As a further stretch goal, add a function that takes in a key name as a string and changes the corresponding value in the object you just made to be the string 'overwritten DYNAMICALLY'. Now call it with the name of one of your keys and see if it works! This will all take some research... Good luck!
+const changeMyOwnObj = function(keyName) {
+  myOwnObj['']
+}
